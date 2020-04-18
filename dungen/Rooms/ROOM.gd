dff7b340167e16e6ea814b021242b17a9d22133f
Shane <shanescott86@gmail.com>
2020-04-17 23:09:53 -0500

    2d arrayyys babeyyyy

--- dungen/Rooms/ROOM.gd
+++ dungen/Rooms/ROOM.gd
@@ -3,7 +3,7 @@ extends Node2D
 class_name room
 
 export var size = Vector2(54,57)
-var ground = [[]]
+var ground = []
 var connected_rooms = []
 
 var tilesize = 128/2
@@ -16,10 +16,11 @@ func _ready():
 
 func generate_floor(size):
 	for xpos in size.x:
+		ground.append([])
 		for ypos in size.y:
 			var t = tile.instance()
 			t.position = roompos + Vector2(tilesize*xpos,tilesize*ypos)
-			ground[size.x][size.y] = t
+			ground[xpos].append(t)
 			add_child(t)
 			
 	
