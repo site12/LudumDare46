86d85cc00e14bedd157c49667440d15fcec0e20f
jkazuk <jeffreykazuk@gmail.com>
2020-04-18 01:50:49 -0500

    dungen done

--- Dungen.gd
+++ Dungen.gd
@@ -45,15 +45,69 @@ func make_rooms():
 	for room in $Rooms.get_children():
 		var new_room = Generated_Room.instance()
 		new_room.size = room.size
+		new_room.pos = room.position
+		new_room.astar_index = path.get_closest_point(Vector3(new_room.pos.x, new_room.pos.y, 0))
 		room_array.append(new_room)
-	for room_id in path.get_points():
-		for linked_room in path.get_point_connections(room_id):
-			room_array[room_id].connected_rooms.append(room_array[linked_room])
+		
+	# for room in room_array:
+	# 	room.astar_index = path.get_closest_point(room.pos)
+	# 	for linked_room in path.get_point_connections(room.astar_index):
+
+
+	# for room_id in path.get_points():
+	# 	var room_pos = path.get_point_position(room_id)
+	# 	for linked_room in path.get_point_connections(room_id):
+	# 		var linked_pos = path.get_point_position(linked_room)
+	# 		var pos_dif = room_pos - linked_pos
+
+
+	for room in room_array:
+		yield(get_tree().create_timer(0.1), 'timeout')
+		for linked_room in path.get_point_connections(room.astar_index):
+			for searched_room in room_array:
+				if searched_room.astar_index == linked_room:
+					var found_room = searched_room
+					var pos_dif = room.pos - found_room.pos
+					if abs(pos_dif.x) > abs(pos_dif.y):
+						if found_room.pos.x < room.pos.x:
+							connect_room(room, found_room, 'left')
+							# room.connected_rooms['left'] = found_room
+							# print(str(room)+' Connected left  to '+str(found_room))
+							# print(str(room.pos)+'\n' + str(found_room.pos)+'\n'+str(pos_dif))
+						else:
+							connect_room(room, found_room, 'right')
+							# room.connected_rooms['right'] = found_room
+							# print(str(room)+' Connected right to '+str(found_room))
+							# print(str(room.pos)+'\n' + str(found_room.pos)+'\n'+str(pos_dif))
+					elif abs(pos_dif.x) < abs(pos_dif.y):
+						if found_room.pos.y < room.pos.y:
+							connect_room(room, found_room, 'down')
+							# room.connected_rooms['down'] = found_room
+							# print(str(room)+' Connected down  to '+str(found_room))
+							# print(str(room.pos)+'\n' + str(found_room.pos)+'\n'+str(pos_dif))
+						else:
+							connect_room(room, found_room, 'up')
+							# room.connected_rooms['up'] = found_room
+							# print(str(room)+' Connected up    to '+str(found_room))
+							# print(str(room.pos)+'\n' + str(found_room.pos)+'\n'+str(pos_dif))
+					else:
+						print('I cant believe youve done this')
+			
 	Level.room_array = room_array
 		
-	print(room_array)
-
+	#print(room_array)
+func connect_room(room, room_to, direction):
+	var directions = ['right', 'up', 'left', 'down']
+	if not room.connected_rooms.has(direction):
+		room.connected_rooms[direction] = room_to
+		print(str(room)+' Connected ' + direction + ' to '+str(room_to))
+	else:
+		if direction == 'down':
+			connect_room(room, room_to, 'right')
+		else:
+			connect_room(room, room_to, directions[directions.find(direction)+1])
 		
+			
 func _draw():
 	for room in $Rooms.get_children():
 		draw_rect(Rect2(room.position - room.size, room.size * 2),
