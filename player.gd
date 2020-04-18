dce77d39f084026f13732a951eec8a23f9a8e3a3
Shane <shanescott86@gmail.com>
2020-04-17 20:05:50 -0500

    movement not so shitty

--- player.gd
+++ player.gd
@@ -16,6 +16,6 @@ func get_input():
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	get_input()
 	velocity = move_and_slide(velocity)
