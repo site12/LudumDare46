b5bf85c19ce234bbeec753cc9b5c8f13e20952c7
Shane <shanescott86@gmail.com>
2020-04-17 22:30:02 -0500

    sdsdsd

--- dungen/Rooms/ROOM.gd
+++ dungen/Rooms/ROOM.gd
@@ -2,23 +2,24 @@ extends Node2D
 
 class_name room
 
-export var sizex = 5
-export var sizey = 5
+export var size = Vector2(54,57)
+var ground = [[]]
 var connected_rooms = []
 
 var tilesize = 128/2
 var roompos = global_position
-onready var tile = $tile
+onready var tile = load("res://dungen/Rooms/floorTile.tscn")
 
-func _init():
-	generate_floor(sizex,sizey)
+func _ready():
+	generate_floor(size)
 	
 
-func generate_floor(x,y):
-	for xpos in sizex:
-		for ypos in sizey:
+func generate_floor(size):
+	for xpos in size.x:
+		for ypos in size.y:
 			var t = tile.instance()
 			t.position = roompos + Vector2(tilesize*xpos,tilesize*ypos)
+			ground[size.x][size.y] = t
 			add_child(t)
 			
 	
