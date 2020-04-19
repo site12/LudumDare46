43edb0f48386c7e805398c9a3614358a7c10b7d1
Shane <shanescott86@gmail.com>
2020-04-19 13:57:05 -0500

    sdasd

--- enemies/shroomguy.gd
+++ enemies/shroomguy.gd
@@ -1,4 +1,14 @@
 extends enemy
 
-
+func _ready():
+	speed = 0
+	$spritehelper.visible = false
+	$hurt.visible = false
+	$CollisionShape2D.disabled = true
+	yield(get_tree().create_timer(2), 'timeout')
+	$CollisionShape2D.disabled = false
+	$spritehelper.visible = true
+	$hurt.visible = true
+	speed = 100
+	visible = true
 
