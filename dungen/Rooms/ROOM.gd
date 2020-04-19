6d8747fca61fc87b5f49c101beaac928b19ec5d0
Shane <shanescott86@gmail.com>
2020-04-18 23:11:08 -0500

    collision babeyyyy

--- dungen/Rooms/ROOM.gd
+++ dungen/Rooms/ROOM.gd
@@ -9,6 +9,13 @@ const EXTRA = preload('res://dungen/Rooms/extratile.tscn')
 const PLAYER = preload('res://player/player.tscn')
 const ENEMY = [preload('res://enemies/shroomguy.tscn')]
 const WALLS = preload('res://dungen/Rooms/walls/wall.tscn')
+const COLL = [
+preload('res://dungen/Rooms/walls/left.tscn'),
+preload('res://dungen/Rooms/walls/right.tscn'),
+preload('res://dungen/Rooms/walls/up.tscn'),
+preload('res://dungen/Rooms/walls/down.tscn')
+
+]
 export var size = Vector2(10,10)
 var current_room = false
 var ground = []
@@ -120,47 +127,59 @@ func generate_walls(size):
 	#backwall
 	for xpos in size.x:
 		var t = WALLS.instance()
+		var c = COLL[3].instance()
 		t.tile = t.tiles[0]
 		t.position = roompos + Vector2(tilesize*xpos,tilesize*size.y+32)
+		c.position = roompos + Vector2(tilesize*xpos,tilesize*size.y+32)
 		add_child(t)
+		add_child(c)
 	
 	#rightwall
 	for ypos in size.y+2:
 		var t = WALLS.instance()
+		var c = COLL[1].instance()
 		if ypos == size.y+1:
 			t.tile = t.corners[3]
 		else:
 			t.tile = t.tiles[1]
 		t.position = roompos + Vector2(tilesize*size.x+tilesize,tilesize*ypos)
+		c.position = roompos + Vector2(tilesize*size.x+tilesize,tilesize*ypos)
 		add_child(t)
+		add_child(c)
 		
 	#leftwall
 	for ypos in size.y+2:
 		var t = WALLS.instance()
+		var c = COLL[0].instance()
 		if ypos == size.y+1:
 			t.tile = t.corners[2]
 		else:
 			t.tile = t.tiles[2]
 		t.position = roompos + Vector2(-tilesize*2,tilesize*ypos)
+		c.position = roompos + Vector2(-tilesize*2,tilesize*ypos)
 		add_child(t)
+		add_child(c)
 	
 	#frontwall
 	for xpos in size.x+2:
 		var t = WALLS.instance()
+		var c = COLL[2].instance()
 		if xpos == size.x+1:
 			t.tile = t.corners[1]
 		else:
 			t.tile = t.tiles[3]
 		t.position = roompos + Vector2(tilesize*xpos,tilesize*-3.75)
+		c.position = roompos + Vector2(tilesize*xpos,tilesize*-3.75)
 		t.z_index = 999
 		add_child(t)
+		add_child(c)
 
 func generate_gaps(size):
 	#treewall
 	for xpos in size.x+1:
 		var t = TILE.instance()
 		t.tree()
-		t.z_index = -1000
+		t.z_index = -999
 		t.position = roompos + Vector2(tilesize*xpos-tilesize/2,-tilesize+5)
 		add_child(t)
 	
