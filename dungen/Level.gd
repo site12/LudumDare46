71d777df9ec480143b535088b19efd41057909d1
Shane <shanescott86@gmail.com>
2020-04-20 12:50:51 -0500

    added the sound of music

--- dungen/Level.gd
+++ dungen/Level.gd
@@ -5,6 +5,7 @@ var room_array = []
 var a_star
 var virtual_rooms = []
 var direction = ""
+var biome = "forest"
 var cursor = load("res://tiles/cursor.png")
 func _ready():
 	Input.set_custom_mouse_cursor(cursor)
---
+++ town/music.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+onready var music = [
+	preload("res://Music/town - sstw.wav"),
+	preload("res://Music/hell - sstw.wav"),
+	preload("res://Music/forest - sstw.wav"),
+	preload("res://Music/cave - sstw.wav"),
+	preload("res://Music/bossfight - sstw.wav")
+
+]
+
+
+# Called when the node enters the scene tree for the first time.
+func play():
+	if Level.biome == "forest":
+		$AudioStreamPlayer2D.set_stream(music[2])
+	if Level.biome == "cave":
+		$AudioStreamPlayer2D.set_stream(music[3])
