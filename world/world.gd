fee3cd8457a752eb2adbb23d4b92f6c4987cabfe
Shane <shanescott86@gmail.com>
2020-04-20 18:31:14 -0500

    hell walls bitch

--- world/world.gd
+++ world/world.gd
@@ -6,6 +6,12 @@ func _ready():
 	#$transition.play(true)
 	$transition.black()
 	$music.play()
+	if Level.biome == 'cave':
+		VisualServer.set_default_clear_color(Color("#302f39"))
+	if Level.biome == 'forest':
+		VisualServer.set_default_clear_color(Color("#dd401a"))
+	if Level.biome == 'hell':
+		VisualServer.set_default_clear_color(Color("8f0030"))
 
 func open():
 	$transition.play(false)
